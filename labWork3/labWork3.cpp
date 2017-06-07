1ae3d7f988ef1224bbef4737b21a030c48709734
Lenovo-root\Root <just1call.odn@gmail.com>
2017-06-08 06:08:53 +0700

    1-2-3 lab_end

--- labWork3/labWork3.cpp
+++ labWork3/labWork3.cpp
@@ -1,32 +1,59 @@
 #include <stdio.h>
 #include <conio.h>
-int digit(char *S, int n)
+
+int stringToDigit(char *c)
 {
-	for (int i = 0; S[i]!= '\0'; i++)
+	int digit = 0;
+	for (int i = 0; c[i] != '\0'; i++)
 	{
-		//int *B = new int[n];
-		int hex = 0;
-		if ('0' <= S[i] && S[i] <= '9')
+		if (c[i] >= '0' && c[i] <= '9')
 		{
-			hex = S[i]%16; 
-			if (hex > 9)
-			{
-				hex += (int)'A' - 10;
-			}
-			else
-			{
-				hex += (int)'0';
-			}		
-			
-		}
-		return hex;
-	}	
+			digit *= 10;
+			digit += c[i] - '0';
+		}				
+	}
+	return digit;
+}
+
+void revers(char *s)
+{
+	int j;
+	for (j = 0; s[j] != '\0'; j++);
+		j--;
+	for (int i = 0; i < j; i++, j--)
+	{
+		char swap = s[i];
+		s[i] = s[j];
+		s[j] = swap;
+	}
+}
+
+void digitToString(int digit, char *s,int n)
+{
+	int a = 0;
+	int j = 0;
+	while (digit!=0)
+	{
+		a = digit % 16;
+		if (a > 9)
+			s[j] = a + '7';
+		else s[j] = a + '0';
+		digit = digit / 16;
+		j++;
+	}
+	s[j] = '\0';
+	revers(s);
 }
 
 int main()
 {
-	const int n = 5;
-	char S[n] = { 100,200,0xA3,200,500 };
-	
+	const int n = 8;
+	char str1[n] = "ABc1450";
+	char str2[n];
+	int a = stringToDigit(str1);
+	digitToString(a, str2, n);
+	printf("%s\n%d\n",str1, a);
+	printf("%s\n",str2);
 	_getch();
+
 }
